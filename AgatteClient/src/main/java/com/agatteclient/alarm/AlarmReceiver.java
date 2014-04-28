afd95a804d55ddef9ed7058ef24d52773c05d070
Rémi Pannequin <remi.pannequin@cran.uhp-nancy.fr>
2014-04-28 13:23:03 +0200

    reactivate actual firing of alarms (d'oh)

--- AgatteClient/src/main/java/com/agatteclient/alarm/AlarmReceiver.java
+++ AgatteClient/src/main/java/com/agatteclient/alarm/AlarmReceiver.java
@@ -58,7 +58,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         final Intent i = new Intent(context, PunchService.class);
         i.setAction(PunchService.DO_PUNCH);
         i.putExtra(PunchService.RESULT_RECEIVER, new PunchResultReceiver(context));
-        //context.startService(i);
+        context.startService(i);
         wl.release();
     }
 
