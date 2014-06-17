b6237f142a6ff305d1317bd1d2c8e2bf41ac2ae8
Rémi Pannequin <remi.pannequin@cran.uhp-nancy.fr>
2014-06-17 17:17:01 +0200

    better display of constraint indicator

--- AgatteClient/src/main/java/com/agatteclient/card/DayCardView.java
+++ AgatteClient/src/main/java/com/agatteclient/card/DayCardView.java
@@ -202,6 +202,7 @@ public class DayCardView extends View {
         alarm_text_paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         alarm_text_paint.setTextSize(text_height);
         alarm_text_paint.setColor(alarm_color);
+        alarm_text_paint.setStyle(Paint.Style.FILL);
 
         alarm_fill_paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         alarm_fill_paint.setStyle(Paint.Style.FILL);
@@ -571,21 +572,21 @@ public class DayCardView extends View {
                           alarm_paint);
 
         //TODO : display smth to show arrival/leaving constraints
-        if (type == PunchAlarmTime.Type.arrival) {
+        if (type == PunchAlarmTime.Type.arrival || type == PunchAlarmTime.Type.leaving) {
             Path alarm_constr_path = new Path();
             alarm_constr_path.reset();
             float required_h = pxToDp(ODD_H / 2);
-            int num = (int) Math.floor(rect_width / (2 * required_h));
-            float real_h = rect_width / (2 * num);
-            alarm_constr_path.rLineTo(0, real_h);
+            int num = (int) Math.floor((rect_width - w - (h / 2) - pad) / (2 * required_h));
+            float real_h = (rect_width - w - (h / 2) - pad) / (2 * num);
+            int f = (type == PunchAlarmTime.Type.leaving ? 1 : -1);
             for (int i = 0; i < num; i++) {
-                alarm_constr_path.rLineTo(real_h, -real_h);
-                alarm_constr_path.rLineTo(real_h, real_h);
+                alarm_constr_path.rLineTo(real_h, -f * real_h);
+                alarm_constr_path.rLineTo(real_h, f * real_h);
             }
-            alarm_constr_path.rLineTo(0, -real_h);
+
             alarm_constr_path.close();
             alarm_constr_path.offset(margin, y);
-            canvas.drawPath(alarm_constr_path, alarm_paint);
+            canvas.drawPath(alarm_constr_path, alarm_text_paint);
         }
     }
 
